f23ae662bd39d1c557287fc054194d00e73d89de
aa.Tessst <781867500@qq.com>
2017-03-13 23:53:36 +0800

    git remote add origin https://github.com/0519LJL/testhelloworld.git

---
+++ testhelloworld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+package  com.jase.HelloWorld;
+/**
+ * Created by Administrator on 2017/3/13 0013.
+ */
+public class HelloWorld {
+
+        public  static void main(String[] args)
+        {
+            System.out.println("our plan go on");
+            System.out.println("helloworld!");
+        }
+
+}
